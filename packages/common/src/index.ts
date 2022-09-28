4a409e52a036d7783cf270a0616ed745b56e8601
woofbotapp <woofbot@protonmail.com>
2022-09-28 23:44:36 +0300

    preparing parameters request messages

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -33,6 +33,7 @@ export enum BotCommandName {
 interface BotCommand {
   command: BotCommandName;
   description: string;
+  parametersRequestMessage?: string;
 }
 
 export const telegramCommands: BotCommand[] = [
@@ -70,10 +71,22 @@ export const telegramCommands: BotCommand[] = [
       'Get notifications when a transaction is found in the mempool, confirmed,',
       'or is being double-spent.',
     ].join(' '),
+    parametersRequestMessage: [
+      'Which transaction-id do you want to watch? You can specify only the id like:',
+      '"a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d",',
+      'or you can add a nickname like:',
+      '"pizza_order:a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d".',
+      'The nickname should not contain spaces.',
+    ].join(' '),
   },
   {
     command: BotCommandName.UnwatchTransactions,
     description: 'Stop getting notifications about one or more transactions.',
+    parametersRequestMessage: [
+      'Which transaction-ids or nicknames of the transactions do you no longer want',
+      'to watch? You can specify multiple values with spaces between them.',
+      'You can also specify only the prefix, with a "*" at the end, like: "a1075db55d416d3c*".',
+    ].join(' '),
   },
   {
     command: BotCommandName.WatchAddresses,
@@ -82,14 +95,27 @@ export const telegramCommands: BotCommand[] = [
       'mempool or confirmed. Get notification when a transaction spending from the given',
       'addresses is confirmed (but not when it is only added to the mempool).',
     ].join(' '),
+    parametersRequestMessage: [
+      'Which addresses do you want to watch? You can specify only the address, like:',
+      '"17SkEw2md5avVNyYgj6RiXuQKNwkXaxFyQ", or you can add a nickname like:',
+      '"pizza_guy:17SkEw2md5avVNyYgj6RiXuQKNwkXaxFyQ".',
+      'You can specify multiple values with spaces between them.',
+      'The nicknames should not contain spaces.',
+    ].join(' '),
   },
   {
     command: BotCommandName.UnwatchAddresses,
     description: 'Stop getting notifications about one or more addresses.',
+    parametersRequestMessage: [
+      'Which addresses or nicknames of addresses do you no longer want to watch?',
+      'You can specify multiple values with spaces between them.',
+      'You can also specify only the prefix, with a "*" at the end, like: "17SkEw2m*".',
+    ].join(' '),
   },
   {
     command: BotCommandName.WatchPriceChange,
     description: 'Watch changes in the price of Bitcoin (in USD).',
+    parametersRequestMessage: 'What price change (in USD) do you want to watch?',
   },
   {
     command: BotCommandName.UnwatchPriceChange,

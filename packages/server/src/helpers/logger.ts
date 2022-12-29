0c0189b2fc587f17d04c2ed2e3d3e28817740d6a
woofbotapp <woofbot@protonmail.com>
2022-12-30 00:19:30 +0200

    delete previous log files

--- packages/server/src/helpers/logger.ts
+++ packages/server/src/helpers/logger.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { createLogger, format, transports } from 'winston';
 
 export const defaultLogFormat = format.combine(
@@ -8,19 +10,73 @@ export const defaultLogFormat = format.combine(
   }): string => `${timestamp} [${label}] ${level}: ${message}`),
 );
 
+interface SuccessFileTransport {
+  kind: 'success';
+  fileTransport: typeof transports.File;
+}
+
+interface FailFileTransport {
+  kind: 'fail';
+  operations: string[];
+  error: unknown;
+}
+
+function createFileTransport(
+  serverLogsFilepath: string,
+): SuccessFileTransport | FailFileTransport {
+  const operations: string[] = [];
+  try {
+    operations.push(`Trying to access ${serverLogsFilepath}`);
+    fs.writeFileSync(serverLogsFilepath, '');
+    const { dir, name, ext } = path.parse(serverLogsFilepath);
+    operations.push(`Listing files in ${dir}`);
+    fs.readdirSync(dir).forEach((filename) => {
+      // checks that the file matchs '<name><digits><ext>'.
+      if (!filename.startsWith(name) || !filename.endsWith(ext)) {
+        return;
+      }
+      const wildcard = filename.slice(name.length, -ext.length ?? Infinity);
+      if (!/^\d*$/.test(wildcard)) {
+        return;
+      }
+      const filepath = path.join(dir, filename);
+      operations.push(`Unlinking ${filepath}`);
+      fs.unlinkSync(filepath);
+    });
+    return {
+      kind: 'success',
+      fileTransport: new transports.File({
+        filename: serverLogsFilepath,
+        maxsize: 10_000_000,
+        maxFiles: 2,
+      }),
+    };
+  } catch (error) {
+    return {
+      kind: 'fail',
+      operations,
+      error,
+    };
+  }
+}
+
+const ftResult = process.env.SERVER_LOGS_FILEPATH ? createFileTransport(
+  process.env.SERVER_LOGS_FILEPATH,
+) : undefined;
+
 const logger = createLogger({
   level: (process.env.NODE_ENV === 'production') ? 'info' : 'debug',
   format: defaultLogFormat,
   transports: [
     new transports.Console(),
-    ...process.env.SERVER_LOGS_FILEPATH ? [
-      new transports.File({
-        filename: process.env.SERVER_LOGS_FILEPATH,
-        maxsize: 10_000_000,
-        maxFiles: 2,
-      }),
-    ] : [],
+    ...ftResult?.kind === 'success' ? [ftResult.fileTransport] : [],
   ],
 });
+
+if (ftResult?.kind === 'fail') {
+  logger.error(`Failed to initialize file logger: ${
+    ftResult.operations.join('\n')
+  } ${ftResult.error}`);
+}
 
 export default logger;
